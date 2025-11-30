aa46c633f64a31cc74dfbe1c699a965425b3811a
Vinicius Bussines <emprearialgrotwh@gmail.com>
2025-11-30 02:52:39 -0300

    errolog

--- app/Http/Controllers/Api/TransactionPixController.php
+++ app/Http/Controllers/Api/TransactionPixController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades.Log;
+use Illuminate\Support\Facades\Log;
 use App\Models\Transaction;
 use App\Models\Withdraw;
 use App\Enums\TransactionStatus;
